510fd340624dd3434a958530e9c16d90fcb721d8
BomberFish <bomberfish@bomberfish.ca>
2025-06-12 08:52:54 -0400

    Initial Commit

---
+++ Pachyderm/PachydermApp.swift
@@ -0,0 +1,17 @@
+//
+//  PachydermApp.swift
+//  Pachyderm
+//
+//  Created by Hariz Shirazi on 2025-06-12.
+//
+
+import SwiftUI
+
+@main
+struct PachydermApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
